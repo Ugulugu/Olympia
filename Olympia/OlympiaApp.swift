f1367c3cbf6f06768f9987a9dc6d83aa8ef6fc22
Max <maximilian@dahos.de>
2021-07-17 16:25:10 +0200

    Initial Commit

---
+++ Olympia/OlympiaApp.swift
@@ -0,0 +1,17 @@
+//
+//  OlympiaApp.swift
+//  Olympia
+//
+//  Created by Maximilian Schröder on 17.07.21.
+//
+
+import SwiftUI
+
+@main
+struct OlympiaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
